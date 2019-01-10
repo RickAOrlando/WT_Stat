1a3d7bf4e6e04f874e96ec32d290798ae0ac9a36
RickAOrlando <RickAOrlando85@gmail.com>
2019-01-10 00:32:53 -0500

    - Added more updated values - Added larger test text

--- src/winratemachine/WRView.java
+++ src/winratemachine/WRView.java
@@ -37,8 +37,10 @@ public class WRView extends javax.swing.JFrame implements ActionListener {
         // Calling timer task method using arguments to pass in the labels 
         // and panels to be changed and updated
         givenUsingTimer_whenSchedulingRepeatedTask_thenCorrect(
-                this.labelPitchValue, this.lableAileronValue, 
+                this.labelHPValue, this.labelThrottleValue, 
+                this.labelPitchValue, this.labelAileronValue, 
                 this.aircraftUpdatePanel);
+        
     }   
 
     @SuppressWarnings("unchecked")
@@ -48,18 +50,22 @@ public class WRView extends javax.swing.JFrame implements ActionListener {
         panelMain = new javax.swing.JPanel();
         buttonWin = new javax.swing.JButton();
         buttonLoss = new javax.swing.JButton();
-        lableWinCounter = new javax.swing.JLabel();
-        lableLossCounter = new javax.swing.JLabel();
-        lableCalculateWinRate = new javax.swing.JLabel();
+        labelWinCounter = new javax.swing.JLabel();
+        labelLossCounter = new javax.swing.JLabel();
+        labelCalculateWinRate = new javax.swing.JLabel();
         buttonWinSubtract = new javax.swing.JButton();
         buttonCalculate = new javax.swing.JButton();
         buttonWinSubtract1 = new javax.swing.JButton();
-        labelAileronValue = new javax.swing.JLabel();
+        removeThisValue = new javax.swing.JLabel();
         aircraftUpdatePanel = new javax.swing.JPanel();
         labelAileronText = new javax.swing.JLabel();
-        lableAileronValue = new javax.swing.JLabel();
+        labelAileronValue = new javax.swing.JLabel();
         labelPitch = new javax.swing.JLabel();
         labelPitchValue = new javax.swing.JLabel();
+        labelThrottleText = new javax.swing.JLabel();
+        labelThrottleValue = new javax.swing.JLabel();
+        labelHPText = new javax.swing.JLabel();
+        labelHPValue = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -69,14 +75,14 @@ public class WRView extends javax.swing.JFrame implements ActionListener {
         buttonLoss.setText("Loss");
         buttonLoss.addActionListener(this);
 
-        lableWinCounter.setFont(new java.awt.Font("Arial Black", 1, 24)); // NOI18N
-        lableWinCounter.setText("0");
+        labelWinCounter.setFont(new java.awt.Font("Arial Black", 1, 24)); // NOI18N
+        labelWinCounter.setText("0");
 
-        lableLossCounter.setFont(new java.awt.Font("Arial Black", 1, 24)); // NOI18N
-        lableLossCounter.setText("0");
+        labelLossCounter.setFont(new java.awt.Font("Arial Black", 1, 24)); // NOI18N
+        labelLossCounter.setText("0");
 
-        lableCalculateWinRate.setFont(new java.awt.Font("Arial Black", 1, 24)); // NOI18N
-        lableCalculateWinRate.setText("0");
+        labelCalculateWinRate.setFont(new java.awt.Font("Arial Black", 1, 24)); // NOI18N
+        labelCalculateWinRate.setText("0");
 
         buttonWinSubtract.setText("-");
         buttonWinSubtract.addActionListener(this);
@@ -87,13 +93,31 @@ public class WRView extends javax.swing.JFrame implements ActionListener {
         buttonWinSubtract1.setText("-");
         buttonWinSubtract1.addActionListener(this);
 
+        aircraftUpdatePanel.setFont(new java.awt.Font("Dialog", 0, 18)); // NOI18N
+
+        labelAileronText.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
         labelAileronText.setText("Aileron: ");
 
-        lableAileronValue.setText("0");
+        labelAileronValue.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
+        labelAileronValue.setText("0");
 
+        labelPitch.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
         labelPitch.setText("Pitch: ");
 
+        labelPitchValue.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
         labelPitchValue.setText("0");
+
+        labelThrottleText.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
+        labelThrottleText.setText("Throttle: ");
+
+        labelThrottleValue.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
+        labelThrottleValue.setText("0");
+
+        labelHPText.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
+        labelHPText.setText("HorsePower: ");
+
+        labelHPValue.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
+        labelHPValue.setText("0");
 
         javax.swing.GroupLayout aircraftUpdatePanelLayout = new javax.swing.GroupLayout(aircraftUpdatePanel);
         aircraftUpdatePanel.setLayout(aircraftUpdatePanelLayout);
@@ -105,12 +129,20 @@ public class WRView extends javax.swing.JFrame implements ActionListener {
                     .addGroup(aircraftUpdatePanelLayout.createSequentialGroup()
                         .addComponent(labelAileronText)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(lableAileronValue))
+                        .addComponent(labelAileronValue))
                     .addGroup(aircraftUpdatePanelLayout.createSequentialGroup()
                         .addComponent(labelPitch)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(labelPitchValue)))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addComponent(labelPitchValue))
+                    .addGroup(aircraftUpdatePanelLayout.createSequentialGroup()
+                        .addComponent(labelThrottleText)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(labelThrottleValue))
+                    .addGroup(aircraftUpdatePanelLayout.createSequentialGroup()
+                        .addComponent(labelHPText)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(labelHPValue)))
+                .addContainerGap(28, Short.MAX_VALUE))
         );
         aircraftUpdatePanelLayout.setVerticalGroup(
             aircraftUpdatePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -118,12 +150,20 @@ public class WRView extends javax.swing.JFrame implements ActionListener {
                 .addContainerGap()
                 .addGroup(aircraftUpdatePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(labelAileronText)
-                    .addComponent(lableAileronValue))
+                    .addComponent(labelAileronValue))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(aircraftUpdatePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(labelPitch)
                     .addComponent(labelPitchValue))
-                .addContainerGap(56, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(aircraftUpdatePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(labelThrottleText)
+                    .addComponent(labelThrottleValue))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(aircraftUpdatePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(labelHPText)
+                    .addComponent(labelHPValue))
+                .addContainerGap(16, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout panelMainLayout = new javax.swing.GroupLayout(panelMain);
@@ -133,42 +173,38 @@ public class WRView extends javax.swing.JFrame implements ActionListener {
             .addGroup(panelMainLayout.createSequentialGroup()
                 .addGroup(panelMainLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(panelMainLayout.createSequentialGroup()
+                        .addGap(64, 64, 64)
+                        .addComponent(removeThisValue))
+                    .addGroup(panelMainLayout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addGroup(panelMainLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(buttonWin)
+                            .addComponent(buttonWinSubtract1))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(panelMainLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(buttonLoss)
+                            .addComponent(labelWinCounter))
+                        .addGap(266, 266, 266)
+                        .addGroup(panelMainLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(labelLossCounter)
+                            .addComponent(buttonWinSubtract)))
+                    .addGroup(panelMainLayout.createSequentialGroup()
                         .addContainerGap()
                         .addComponent(buttonCalculate)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(lableCalculateWinRate))
-                    .addGroup(panelMainLayout.createSequentialGroup()
-                        .addGroup(panelMainLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, panelMainLayout.createSequentialGroup()
-                                .addGap(64, 64, 64)
-                                .addComponent(labelAileronValue))
-                            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, panelMainLayout.createSequentialGroup()
-                                .addGap(18, 18, 18)
-                                .addGroup(panelMainLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(buttonWin)
-                                    .addComponent(buttonWinSubtract1))
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(lableWinCounter)))
-                        .addGap(18, 18, 18)
                         .addGroup(panelMainLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(panelMainLayout.createSequentialGroup()
-                                .addComponent(buttonLoss)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(lableLossCounter))
-                            .addComponent(buttonWinSubtract)))
-                    .addGroup(panelMainLayout.createSequentialGroup()
-                        .addGap(29, 29, 29)
-                        .addComponent(aircraftUpdatePanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(265, Short.MAX_VALUE))
+                            .addComponent(labelCalculateWinRate)
+                            .addComponent(aircraftUpdatePanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap(23, Short.MAX_VALUE))
         );
         panelMainLayout.setVerticalGroup(
             panelMainLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelMainLayout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(labelAileronValue)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(removeThisValue)
+                .addGap(114, 114, 114)
                 .addComponent(aircraftUpdatePanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 185, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 41, Short.MAX_VALUE)
                 .addGroup(panelMainLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(buttonWinSubtract1)
                     .addComponent(buttonWinSubtract))
@@ -178,12 +214,12 @@ public class WRView extends javax.swing.JFrame implements ActionListener {
                         .addGroup(panelMainLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(buttonWin)
                             .addComponent(buttonLoss)
-                            .addComponent(lableLossCounter))
+                            .addComponent(labelLossCounter))
                         .addGap(18, 18, 18)
                         .addGroup(panelMainLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(lableCalculateWinRate)
+                            .addComponent(labelCalculateWinRate)
                             .addComponent(buttonCalculate)))
-                    .addComponent(lableWinCounter))
+                    .addComponent(labelWinCounter))
                 .addContainerGap())
         );
 
@@ -229,21 +265,21 @@ public class WRView extends javax.swing.JFrame implements ActionListener {
 
     
     private void buttonCalculateActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonCalculateActionPerformed
-        double wc = Integer.parseInt(this.lableWinCounter.getText());
-        double lc = Integer.parseInt(this.lableLossCounter.getText());
+        double wc = Integer.parseInt(this.labelWinCounter.getText());
+        double lc = Integer.parseInt(this.labelLossCounter.getText());
         double total = wc + lc;
         double calc = wc / total;
-        this.lableCalculateWinRate.setText(calc + "");
+        this.labelCalculateWinRate.setText(calc + "");
     }//GEN-LAST:event_buttonCalculateActionPerformed
 
     private void buttonWinActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonWinActionPerformed
         winCounter = winCounter += 1;
-        this.lableWinCounter.setText("" + winCounter);
+        this.labelWinCounter.setText("" + winCounter);
     }//GEN-LAST:event_buttonWinActionPerformed
 
     private void buttonLossActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonLossActionPerformed
         lossCounter = lossCounter += 1;
-        this.lableLossCounter.setText("" + lossCounter);
+        this.labelLossCounter.setText("" + lossCounter);
     }//GEN-LAST:event_buttonLossActionPerformed
 
     private void buttonLossSubtractActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonLossSubtractActionPerformed
@@ -251,7 +287,7 @@ public class WRView extends javax.swing.JFrame implements ActionListener {
             // do nothing
         } else {
             lossCounter = lossCounter -= 1;
-            this.lableLossCounter.setText("" + lossCounter);
+            this.labelLossCounter.setText("" + lossCounter);
         }
     }//GEN-LAST:event_buttonLossSubtractActionPerformed
 
@@ -260,14 +296,18 @@ public class WRView extends javax.swing.JFrame implements ActionListener {
             // do nothing
         } else {
             winCounter = winCounter -= 1;
-            this.lableWinCounter.setText("" + winCounter);
+            this.labelWinCounter.setText("" + winCounter);
         }
     }//GEN-LAST:event_buttonWinSubtract1ActionPerformed
 
     // Timer task method
     // This method is where you add arguments as JLabels, then use that argument
     // to set the text on that label
-    public void givenUsingTimer_whenSchedulingRepeatedTask_thenCorrect(JLabel labelPitchValue, JLabel labelAileronValue, JPanel aircraftUpdatePanel){
+    public void givenUsingTimer_whenSchedulingRepeatedTask_thenCorrect(
+            JLabel labelHPValue, JLabel labelThrottleValue, 
+            JLabel labelPitchValue, JLabel labelAileronValue, 
+            JPanel aircraftUpdatePanel){
+        
     TimerTask repeatedTask;
         repeatedTask = new TimerTask() {
             @Override
@@ -279,6 +319,8 @@ public class WRView extends javax.swing.JFrame implements ActionListener {
             Logger.getLogger(WRView.class.getName()).log(Level.SEVERE, null, ex);
         }
             // This is where you use the argument to set the label
+            labelHPValue.setText(controller.parseString("power 1, hp").getAsString());
+            labelThrottleValue.setText(controller.parseString("RPM throttle 1, %").getAsString());
             labelPitchValue.setText(controller.parseString("pitch 1, deg").getAsString());
             labelAileronValue.setText(controller.parseString("aileron, %").getAsString());
             aircraftUpdatePanel.revalidate();
@@ -303,14 +345,18 @@ public class WRView extends javax.swing.JFrame implements ActionListener {
     private javax.swing.JButton buttonWinSubtract;
     private javax.swing.JButton buttonWinSubtract1;
     private javax.swing.JLabel labelAileronText;
-    private javax.swing.JLabel labelAileronValue;
+    public javax.swing.JLabel labelAileronValue;
+    private javax.swing.JLabel labelCalculateWinRate;
+    private javax.swing.JLabel labelHPText;
+    private javax.swing.JLabel labelHPValue;
+    private javax.swing.JLabel labelLossCounter;
     private javax.swing.JLabel labelPitch;
     private javax.swing.JLabel labelPitchValue;
-    public javax.swing.JLabel lableAileronValue;
-    private javax.swing.JLabel lableCalculateWinRate;
-    private javax.swing.JLabel lableLossCounter;
-    private javax.swing.JLabel lableWinCounter;
+    private javax.swing.JLabel labelThrottleText;
+    private javax.swing.JLabel labelThrottleValue;
+    private javax.swing.JLabel labelWinCounter;
     private javax.swing.JPanel panelMain;
+    private javax.swing.JLabel removeThisValue;
     // End of variables declaration//GEN-END:variables
 
     // Manual variable declaration   
