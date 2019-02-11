dc2582b40baf8953500db18e27439da9cf30f59e
RickAOrlando <RickAOrlando85@gmail.com>
2019-02-11 17:23:29 -0500

    - Added TODO file to track upcoming implementations

---
+++ src/files/TODO.java
@@ -0,0 +1,26 @@
+/*
+ * Last Updated: 0/0/2017
+ * Class description:
+ */
+package files;
+
+/**
+ *
+ * @author Pussy Whisperer
+ */
+public class TODO {
+    /*
+    Make reset button
+        - Reset values to default settings
+    */
+    
+    /*
+    Make save button
+        - Allow for the saving of a session to a text doc
+    */
+        
+    /*
+    Make restore button
+        - Allow for restoration of last session so you can continue tracking
+    */
+}
